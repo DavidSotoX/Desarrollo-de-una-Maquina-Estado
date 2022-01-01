9144afa57be82374d7b91cccc8cdf66985cd8c95
DavidSotoX <94983482+DavidSotoX@users.noreply.github.com>
2022-01-01 18:25:56 -0500

    Add files via upload

---
+++ ejericio1/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "secureip" -o "E:/Programas/Xilinx/TareaMaquinaEstados/ejericio1/MooreCartaASM_isim_beh.exe" -prj "E:/Programas/Xilinx/TareaMaquinaEstados/ejericio1/MooreCartaASM_beh.prj" "work.MooreCartaASM" 
